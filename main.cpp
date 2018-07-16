7f8e05e3fa28c88000b353df0b80ad6bc29f6283
0obriano0 <brian041301@gmail.com>
2018-07-16 22:30:37 +0800

    create * & +

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@
 
 /* 算法運算區 */ 
 char* add(char *num1,char *num2);
+char* multiply(char *num1,char *num2);
+char* divide(char *num1,char *num2);
 /* 字串處理區 */
 void string_main(char *str);
 void check_negative(char *str);
+void check_multiply_and_divide(char *str);
 void check_add(char *str);
 int get_string_length(char *str);
 void Remake(char *str);
+void take_out_space(char* str);
 void get_parentheses(char *str);
 bool check_parentheses(char *str);
 /* 錯誤回報 */
@@ -46,12 +50,28 @@ char* add(char *num1,char *num2){
 	sprintf(num,"%lf",atof(num1)+atof(num2));
 	return num;
 }
+char* multiply(char *num1,char *num2){
+	char num[100];
+	sprintf(num,"%lf",atof(num1)*atof(num2));
+	return num;
+}
+
+char* divide(char *num1,char *num2){
+	char num[100];
+	if(atof(num2) == 0)
+		error(10);
+	sprintf(num,"%lf",atof(num1)/atof(num2));
+	return num;
+}
+
 
 /* 字串處理區 */
 void string_main(char *str){				//檢查的主函式
 	get_parentheses(str);
 	check_negative(str);
+	check_multiply_and_divide(str);
 	check_add(str);
+	take_out_space(str);
 }
 
 void check_negative(char *str){				// - 的判定 
@@ -73,6 +93,79 @@ void check_negative(char *str){				// - 的判定
 			}else{
 				if(*(str+str_index[1]-1)!='+')
 					error(-1);
+			}
+		}
+		str_index[1]++;
+	}
+}
+
+void check_multiply_and_divide(char *str){
+	int str_index[2]={0,0};
+	char str_num[2][str_length+1];
+	str_num[0][0] = '\0';
+	str_num[1][0] = '\0';
+	int load_num[2];
+	str_index[1] = 0;
+	load_num[0] = 0;
+	load_num[1] = 0;
+	int mode = 0;
+	char get_code = '\0';
+	while(!*(str+str_index[1]) == '\0'){
+		if(get_code == '\0' && mode == 0 && (*(str+str_index[1]) == '*' || *(str+str_index[1]) == '/')){
+			get_code = *(str+str_index[1]);
+			mode = 1;
+			str_index[0] = str_index[1]-1;
+		}else if(mode == 1){
+			if((*(str+str_index[1])-'0' >=0 && *(str+str_index[1]) - '0'<=9)||*(str+str_index[1]) == '.' || *(str+str_index[1]) == '-'){
+				str_num[1][load_num[1]] = *(str+str_index[1]);
+				load_num[1]++;
+				str_num[1][load_num[1]] = '\0';
+			}else{
+				mode = 2;
+				str_index[1]--;
+			}
+		}else if(mode == 2){
+			while(str_index[0] >= -1 && mode == 2){
+				if(str_index[0] >= 0 && ((*(str+str_index[0])-'0' >=0 && *(str+str_index[0]) - '0'<=9)||*(str+str_index[0]) == '.' || *(str+str_index[0]) == '-')){
+					str_num[0][load_num[0]+1] = '\0';
+					if(load_num[0] != 0){
+						for(int loopnum1 = load_num[0]; loopnum1 > 0; loopnum1--){
+							str_num[0][loopnum1] = str_num[0][loopnum1-1];
+						}
+					}
+					str_num[0][0] = *(str+str_index[0]);
+					load_num[0]++;
+				}else{
+					if(str_index[0] == -1){
+						str_index[0] = 0;
+					}else if (str_index[0] < -1){
+						error(11);
+					}
+					
+					char str_buffer[str_length];
+					str_buffer[0] = '\0';
+					
+					if(get_code == '*'){
+						strcat(str_buffer,multiply(str_num[0],str_num[1]));
+					}else if(get_code == '/'){
+						strcat(str_buffer,divide(str_num[0],str_num[1]));
+					}else{
+						error(12);
+					}
+					strcat(str_buffer, str+str_index[1]);
+					*(str+str_index[0]) = '\0';
+					strcat(str, str_buffer);
+					str_index[1] = 0;
+					str_index[0] = 0;
+					load_num[0] = 0;
+					load_num[1] = 0;
+					str_num[0][0] = '\0';
+					str_num[1][0] = '\0';
+					get_code = '\0';
+					mode = 0;
+					continue;
+				}
+				str_index[0]--;
 			}
 		}
 		str_index[1]++;
@@ -111,8 +204,7 @@ void check_add(char *str){					// + 的判定
 				get_code = '\0';
 				continue;
 			}else{
-				if(*(str+str_index[1]-1)!='+')
-					error(3);
+				error(3);
 			}
 		}
 		str_index[1]++;
@@ -143,6 +235,22 @@ void Remake(char *str){
 	check_parentheses(str);
 }
 
+void take_out_space(char* str){
+	char str_get[100];
+	int loopnum1 = 0;
+	int loopnum2 = 0;
+	while(!*(str+loopnum1)=='\0'){
+		if(*(str+loopnum1)!=' '){
+			str_get[loopnum2] = *(str+loopnum1);
+			loopnum2++;
+		}
+		loopnum1++; 
+	}
+	str_get[loopnum2] = '\0';
+	*str = '\0';
+	strcat(str, str_get);
+}
+
 void get_parentheses(char *str){
 	char str_buffer[str_length];
 	int str_buffer_index = 0;
@@ -160,6 +268,7 @@ void get_parentheses(char *str){
 		}else if(*(str+str_index[1])==')'){
 			parentheses--;
 			if(parentheses == 0){
+				Remake(str_buffer);
 				string_main(str_buffer);
 				strcat(str_buffer, str+str_index[1]+1);
 				*(str+str_index[0]) = '\0';
@@ -206,6 +315,9 @@ void error(int code){
 			break;
 		case 3:
 			printf("+的處裡出現問題");
+			break;
+		case 10:
+			printf("因為運算中出現除與 0 所以程式強制關閉");
 			break;
 		default:
 			printf("未知錯誤，請聯絡程式設計師");
